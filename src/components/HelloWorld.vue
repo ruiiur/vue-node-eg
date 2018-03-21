be0f42a96737078c5d9be833614c42497e24d02c
ruiiur <2362641334@qq.com>
2018-03-21 16:56:00 +0800

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,150 @@
+<!--<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>-->
+<template>
+  <div>
+    <input class="form-control" id="inputEmail3" placeholder="请输入账号" v-model="account">
+    <input type="password" class="form-control" id="inputPassword3" placeholder="请输入密码" v-model="password">
+    <button type="submit" class="btn btn-default" @click="login">登录</button>
+    <loading></loading>
+  </div>
+</template>
+
+<script>
+   import Loading from './loading' 
+    export default {
+    data() {
+        return {
+            account : '',
+            password : ''
+        }
+    },
+    methods:{
+      login() {
+        // 获取已有账号密码
+        this.$http.get('http://localhost:3000/123')
+          .then((response) => {
+            console.log(response);
+            this.account=response.data[0].data;
+          })
+          .catch((reject) => {
+            console.log(reject)
+          });
+        }
+      },
+      mounted:function(){
+        // this.init();
+      },
+      components:{
+        Loading
+      }
+    }
+</script>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import vuex from 'vuex'
+import axios from 'axios'
+Vue.prototype.$http = axios
+Vue.use(vuex);
+var store = new vuex.Store({//store对象
+    state:{
+        show:false
+    }
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
